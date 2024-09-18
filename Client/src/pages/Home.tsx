c11b5465cb88d6a2ab76659eecb75af56902a58e
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-09-18 13:43:03 -0600

    landing page styling, mobile

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import Carousel from '../components/Carousel';
 import SearchBar from '../components/SearchBar';
+import '../utils/Home.css';
 
 const Home = () => {
 
   return (
-    <>
+    <div style={{ height: '700px' }} className="d-flex flex-column ">
       <h2>Where wings take dream</h2>
-      <div>
+      <div className="flex-grow-2 pt-3">
       <SearchBar />
       </div>
-      <div>
+      <div className="flex-grow-3 pt-5">
       <Carousel />
       </div>
-    </>
+    </div>
   );
 };
 
