79c6511fdc9e2e638c5da2ae8a5380a67561756c
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-09-19 13:59:22 -0600

    style landing page

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div style={{ height: '' }} className="flair d-flex flex-column">
       <h2>Redefining the Way You Shop for Tech</h2>
-      <div className="flex-grow-2 pt-3">
+      <div className="flex-grow-2 pt-3 searchBar">
       <SearchBar />
       </div>
       <div className="flex-grow-3 pt-5">
