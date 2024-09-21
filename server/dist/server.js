3f4f662b262339f6d4ea30cfcd4c8d724c614240
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-09-21 15:22:55 -0600

    auth logic

--- server/dist/server.js
+++ server/dist/server.js
@@ -2,12 +2,14 @@ const forceDatabaseRefresh = false; // Flag to control whether to force a databa
 import express from 'express';
 import sequelize from './config/connection.js'; // Import the initialized Sequelize instance
 import routes from './routes/index.js'; // Import the routes for handling different endpoints
+import authMiddleware from './middleware/auth.js'; // Import the authentication middleware
 const app = express(); // Create an Express application
 const PORT = process.env.PORT || 3001; // Define the port for the server to listen on
 // Serves static files from the client's dist folder, typically for a built React application
 app.use(express.static('../client/dist'));
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(routes); // Use the imported routes for handling API endpoints
+app.use('/api/auth-routes/register', authMiddleware); // Apply the authentication middleware to the users endpoint
 // Sync the Sequelize models with the database
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
     app.listen(PORT, () => {
