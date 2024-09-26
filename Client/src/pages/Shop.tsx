25cc21cd6a726621fe7b8f6a5e948edb9d8c5491
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-09-26 15:58:21 -0600

    debugging search bar for render deploy

--- Client/src/pages/Shop.tsx
+++ Client/src/pages/Shop.tsx
@@ -7,8 +7,9 @@ const Shop = () => {
   return (
     <div style={{ height: '' }} className="flair d-flex flex-column">
       <h2>Redefining the Way You Shop for Tech</h2>
+      {/* SearchBar that sticks to the top */}
       <div className="flex-grow-2 pt-3 searchBar">
-      <SearchBar />
+      <SearchBar onSearch={(query: string) => console.log(query)} />
       </div>
       <div className="flex-grow-3 pt-5 carousel">
       <Carousel />
